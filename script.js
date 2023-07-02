b9e37baa0bdebfd68b90e5a05e0db86342bbb57d
Ishwari Mandlik <133854926+isha13m@users.noreply.github.com>
2023-07-02 23:55:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Optional: Add JavaScript functionality if needed for interactivity or dynamic content.
+// This can include things like form validation, smooth scrolling, sliders, etc.
+const linkedinBtn = document.getElementById('linkedin-btn');
+linkedinBtn.addEventListener('click', () => {
+  window.location.href = 'https://www.linkedin.com/in/ishwari-mandlik'; 
+});
+const githubBtn = document.getElementById('github-Btn');
+githubBtn.addEventListener('click', () => {
+  window.location.href = 'https://github.com/isha13m'; 
+});
+const gitHubbtn = document.getElementById('gitHub-btn');
+gitHubbtn.addEventListener('click', () => {
+  window.location.href = 'https://github.com/isha13m?tab=repositories'; 
+});
+const docworkspaceBtn = document.getElementById('docworkspace-btn');
+docworkspaceBtn.addEventListener('click', () => {
+  window.location.href = 'https://in.docworkspace.com/d/sIKXf1rvWAZb4hqUG'; 
+});
+
+  
